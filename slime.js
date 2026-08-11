509dbeaf67b9056f49c4da50a0a92586808652a1
Kyuu92 <k.yuu.2026@gmail.com>
2026-08-12 03:08:10 +0900

    調整

--- slime.js
+++ slime.js
@@ -1,16 +1,22 @@
-// 最初のステータス（変数といいます）
+// 最初のステータス、状態管理
 let hunger = 100;
 let energy = 100;
 
-// 画面を更新する命令
+// 画面を更新、現在ステータスを反映させる
 function updateScreen() {
+
   // 数字を画面に反映させる
   document.getElementById("hungerText").innerText = hunger;
   document.getElementById("energyText").innerText = energy;
   
-  // スライムの様子を変えるルール
+
+  // スライムの様子を変える
+  //ステータスによって絵文字を変える
   let pet = document.getElementById("pet");
   
+
+
+  //０以下はゲームオーバー、３０以下はピンチ、それ以外は元気
   if (hunger <= 0 || energy <= 0) {
     pet.innerText = "👻"; // どっちかが0になったらゲームオーバー
     alert("ゲームオーバー…スライムはいなくなりました。");
@@ -21,26 +27,35 @@ function updateScreen() {
   }
 }
 
-// 「ごはんをあげる」ボタンを押したときの命令
+// 「ごはんをあげる」ボタンを押したとき
 function feed() {
   if (hunger <= 0 || energy <= 0) return; // ゲームオーバーなら何もしない
   
   hunger += 20; // おなかが20回復
-  if (hunger > 100) hunger = 100; // 100を超えないようにする
+  if (hunger > 100) hunger = 100; // 上限は100
   
+
   energy -= 10; // 食べるのも疲れるので、げんきが10減る
   
-  updateScreen(); // 画面を更新！
+
+  updateScreen(); // 画面を更新
 }
 
-// 「ねむらせる」ボタンを押したときの命令
+
+
+
+// 「ねむる」ボタンを押したとき
 function sleep() {
   if (hunger <= 0 || energy <= 0) return; // ゲームオーバーなら何もしない
   
   energy += 30; // げんきが30回復
-  if (energy > 100) energy = 100; // 100を超えないようにする
+  if (energy > 100) energy = 100; // 上限は100
   
-  hunger -= 15; // 寝てる間におなかが15減る
+
   
-  updateScreen(); // 画面を更新！
+  hunger -= 15; // 寝てる間はおなかが15減る
+  
+
+
+  updateScreen(); // 画面を更新
 }
